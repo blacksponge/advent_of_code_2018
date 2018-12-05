36e791f94fd7d555ed3f686d45105e378d34da4e
Gaël Berthaud-Müller <blacksponge@tuta.io>
2018-12-05 13:26:30 +0100

    Finished day 4

---
+++ src/day4_part2.rs
@@ -0,0 +1,106 @@
+extern crate chrono;
+
+use std::io;
+use std::io::BufRead;
+use std::collections::HashMap;
+
+use chrono::{ NaiveDateTime, Timelike };
+
+#[derive(Debug)]
+enum Action {
+  FallsAsleep,
+  WakesUp,
+  BeginShift(i32)
+}
+
+#[derive(Debug)]
+struct TimedAction {
+  action: Action,
+  time: NaiveDateTime
+}
+
+fn main() {
+  let stdin = io::stdin();
+  let reader = stdin.lock();
+  
+  let mut guards: HashMap<i32, Vec<(NaiveDateTime, NaiveDateTime)>> = HashMap::new();
+  let mut guards_vec: Vec<TimedAction> = Vec::new();
+
+  for line in reader.lines() {
+    let mut input = String::from(line.expect("failed to read line"));
+
+    let mut action = input.split_off(17);
+    action = action.split_off(2);
+    let splited_action: Vec<_> = action.split_whitespace().collect();
+    
+    let dt = NaiveDateTime::parse_from_str(&input, "[%Y-%m-%d %H:%M").unwrap();
+
+    let action = match splited_action[0] {
+      "Guard" => {
+        let id_str = &splited_action[1][1..splited_action[1].len()];
+        let id = id_str.parse::<i32>().unwrap();
+        Some(Action::BeginShift(id))
+      },
+      "falls" => Some(Action::FallsAsleep),
+      "wakes" => Some(Action::WakesUp),
+      _ => None
+    };
+
+    let t_act = TimedAction {
+      action: action.expect("unknown action"),
+      time: dt
+    };
+
+    guards_vec.push(t_act);
+  }
+  
+  guards_vec.sort_unstable_by_key(|e| e.time);
+
+  let mut guards_iter = guards_vec.iter().peekable();
+  
+  let mut start_time: NaiveDateTime = NaiveDateTime::from_timestamp(0, 0);
+  let mut naps: Vec<(NaiveDateTime, NaiveDateTime)> = Vec::new();
+  let mut guard_id: i32 = 0;
+
+  loop {
+    let act = match guards_iter.next() {
+      Some(x) => x,
+      None => break
+    };
+
+    match act.action {
+      Action::BeginShift(id) => {
+        guard_id = id;
+      },
+      Action::FallsAsleep => { start_time = act.time; },
+      Action::WakesUp => { naps.push((start_time, act.time)) }
+    };
+
+    match guards_iter.peek() {
+      Some(TimedAction { action: Action::BeginShift(_), time: _}) | None => {
+        guards.entry(guard_id).and_modify(|v| v.append(&mut naps)).or_insert(naps);
+        naps = Vec::new();
+      },
+      _ => ()
+    };
+  }
+ 
+  let best = guards
+    .iter()
+    .filter_map(|(id, naps)| {
+      let mut min_freq: HashMap<u32, u32> = HashMap::new();
+      let minutes = naps.iter().flat_map(|(start, end)| {
+        let s: Vec<_> = (start.minute()..end.minute()).collect();
+        s
+      });
+      for m in minutes {
+        min_freq.entry(m).and_modify(|e| *e += 1).or_insert(1);
+      }
+      if let Some(max) =  min_freq.iter().max_by_key(|e| e.1) {
+        Some((id, *max.0, *max.1))
+      } else {
+        None
+      }
+    }).max_by_key(|e| e.2).unwrap();
+  println!("{:?}", *best.0 * (best.1 as i32));
+}
